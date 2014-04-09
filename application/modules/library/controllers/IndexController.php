2812ff359040c0e58989fa045db37ecc8648149e
californiavol <charlesbrownroberts@gmail.com>
2014-04-08 20:07:29 -0700

    started work on library controllers actions views
    added osmfplayer and related videos mockup to index

--- application/modules/library/controllers/IndexController.php
+++ application/modules/library/controllers/IndexController.php
@@ -5,7 +5,8 @@ class Library_IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+    	//set different layout
+    	$this->_helper->layout->setLayout('library-layout');
     }
 
     public function indexAction()
