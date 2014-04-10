0c2e75e094b168bc6b96f567b112f3ed6f652192
californiavol <charlesbrownroberts@gmail.com>
2014-04-10 10:09:06 -0700

    added playlist layout
    TODO: controller not finding playlist model

--- application/modules/playlist/controllers/IndexController.php
+++ application/modules/playlist/controllers/IndexController.php
@@ -5,12 +5,20 @@ class Playlist_IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+    	//set different layout
+    	$this->_helper->layout->setLayout('playlist-layout');
+    	
+    	//get playlists table
+		$this->db_table = new Playlist_Model_DbTable_Playlists();
     }
 
     public function indexAction()
     {
         // action body
+        
+    	$playlists = $this->db_table->listitems();
+    	$this->view->playlists = $playlists;
+    	
     }
 
 
---
+++ application/modules/playlist/models/DbTable/Playlists.php
@@ -0,0 +1,10 @@
+<?php
+
+class Playlist_Model_DbTable_Playlists extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'playlists';
+
+
+}
+
