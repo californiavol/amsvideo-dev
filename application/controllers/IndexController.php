bb299570f23dbf7bf841cea70e6a8642c0bfbedc
californiavol <charlesbrownroberts@gmail.com>
2014-05-23 10:20:55 -0700

    removed car config from controller

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -123,44 +123,7 @@ class IndexController extends Zend_Controller_Action
         // action body
     }
 
-    public function phpcasAction()
-    {
-        // Load the settings 
-                                        		$cas_host = 'testcas.csus.edu';
-                                        		$cas_port = 443;
-                                        		$cas_context = '/csus.cas';
-                                        		$cas_server_ca_cert_path = 'serviceValidate';
-                                        		// Load the CAS lib
-                                        		require_once 'CAS.php';
-                                        		
-                                        		// Uncomment to enable debugging
-                                        		phpCAS::setDebug();
-                                        		
-                                        		// Initialize phpCAS
-                                        		phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
-                                        		
-                                        		// For production use set the CA certificate that is the issuer of the cert
-                                        		// on the CAS server and uncomment the line below
-                                        		phpCAS::setCasServerCACert($cas_server_ca_cert_path, false);
-                                        		
-                                        		// For quick testing you can disable SSL validation of the CAS server.
-                                        		// THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
-                                        		// VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
-                                        		//phpCAS::setNoCasServerValidation();
-                                        		
-                                        		// force CAS authentication
-                                        		phpCAS::forceAuthentication();
-                                        		
-                                        		// at this step, the user has been authenticated by the CAS server
-                                        		// and the user's login name can be read with phpCAS::getUser().
-                                        		
-                                        		$this->view->phpCAS = phpCAS;
-                                        		
-                                        		// logout if desired
-                                        		if (isset($_REQUEST['logout'])) {
-                                        			phpCAS::logout();
-                                        		}
-    }
+    
 
     public function outputlinksAction()
     {
